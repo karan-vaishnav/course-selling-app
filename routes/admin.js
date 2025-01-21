a1cbbd31c48c851716c9d60f47bdc4ef02b0bd2c
Karan Vaishnav <krn.vaishnav11@gmail.com>
2025-01-21 21:19:34 +0530

    added admin router endpoints

---
+++ routes/admin.js
@@ -0,0 +1,37 @@
+const { Router } = require("express");
+
+const adminRouter = Router();
+
+adminRouter.post("/signup", function (req, res) {
+  res.json({
+    message: "Signup admin endpoint",
+  });
+});
+
+adminRouter.post("/login", function (req, res) {
+  res.json({
+    message: "Login admin endpoint",
+  });
+});
+
+adminRouter.post("/course", function (req, res) {
+  res.json({
+    message: "admin create post endpoint",
+  });
+});
+
+adminRouter.put("/course", function (req, res) {
+  res.json({
+    message: "admin edit post endpoint",
+  });
+});
+
+adminRouter.get("/courses", function (req, res) {
+  res.json({
+    message: "admin get all courses endpoint",
+  });
+});
+
+module.exports = {
+  adminRouter: adminRouter,
+};
